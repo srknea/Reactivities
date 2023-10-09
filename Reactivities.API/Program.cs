0c9f6b2ee894ad5df4042b0019df74f42b9e32e7
Serkan IŞIK <dehnas79@gmail.com>
2023-10-09 14:16:43 +0300

    Seeding data to the Database

--- Reactivities.API/Program.cs
+++ Reactivities.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Reactivities.Persistence;
 
@@ -27,5 +28,20 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
