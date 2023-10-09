a7ec2d567e824780065a7fe76b1c000fe5cde394
Serkan IŞIK <dehnas79@gmail.com>
2023-10-09 20:20:23 +0300

    Fetching data from the API - CORS Policy

--- Reactivities.API/Program.cs
+++ Reactivities.API/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"));
 });
 
+builder.Services.AddCors(opt =>{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
