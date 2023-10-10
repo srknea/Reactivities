a686aa6280625a7ed170c5c583ee261b6b032f18
Serkan IŞIK <dehnas79@gmail.com>
2023-10-10 17:07:08 +0300

    Creating an activity dashboard

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid, List, ListItem } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activities: Activity[];
+}
+
+
+export default function ActivityDashboard({activities}: Props) {
+    return(
+        <Grid>
+            <Grid.Column width='10'>
+                <List>
+                    {activities.map(activity => (
+                        <ListItem key={activity.id}>
+                            {activity.title}
+                        </ListItem>
+                    ))}
+                </List>
+            </Grid.Column>
+        </Grid>
+    )
+}
