d83c85853520b3e6b324a7ba282b948c2984ee68
Serkan IŞIK <dehnas79@gmail.com>
2023-10-10 16:14:15 +0300

    Adding a Nav bar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header, List, ListItem } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -15,7 +16,8 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
+      <NavBar/>
+
       <List>
         {activities.map(activity => (
           <ListItem key={activity.id}>{activity.title}</ListItem>
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export default function NavBar() {
+    return (
+    <div>
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo"/>
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name='Activities'/>
+                <Menu.Item>
+                    <Button positive content="Create Activity"/>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    </div>
+    )
+}
