c62d00821a363064bb379ddac22e75e9dc4d5c5b
Serkan IŞIK <dehnas79@gmail.com>
2023-10-10 09:47:11 +0300

    Thin controllers in the API

--- Reactivities.API/Controllers/ActivitiesController.cs
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -7,17 +7,10 @@ namespace Reactivities.API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         /*
