c035488d01da11f2490bd87de53084527867b6ec
Serkan IŞIK <dehnas79@gmail.com>
2023-10-11 06:56:52 +0300

    Displaying the create/edit form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -10,10 +10,13 @@ interface Props {
     selectedActivity: Activity | undefined;
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
 }
 
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity}: Props) {
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm}: Props) {
     return(
         <Grid>
             <Grid.Column width='10'>
@@ -21,8 +24,13 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
             </Grid.Column>
             
             <Grid.Column width='6'>
-                {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}/>}
-                <ActivityForm/>
+                {selectedActivity && !editMode &&
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm}
+                />}
+                {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity}/>}
             </Grid.Column>
         </Grid>
     )
