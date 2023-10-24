baaffecfa32a688890061ff31a69aefa03c57100
Serkan IŞIK <dehnas79@gmail.com>
2023-10-24 12:10:39 +0300

    Using an interceptor to handle API error responses

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Activity } from '../models/activity';
+import { toast } from 'react-toastify';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -11,14 +12,29 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-axios.interceptors.response.use( async response => {
-    try {
-        await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+axios.interceptors.response.use( async response => { 
+    await sleep(1000);
+    return response;    
+}, (error : AxiosError) => {
+    const {data, status} = error.response!;
+    switch (status) {
+        case 400:
+            toast.error('bad request');
+            break;
+        case 401:
+            toast.error('unauthorized');
+            break;
+        case 403:
+            toast.error('forbidden');
+            break;    
+        case 404:
+            toast.error('not found');
+            break;
+        case 500:
+            toast.error('server error');
+            break;
     }
+    return Promise.reject(error);
 })  
 
 const requests = {
