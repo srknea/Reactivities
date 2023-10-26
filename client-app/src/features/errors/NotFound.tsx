7cee7e318b433c0b9d945a68368ab8e1a285926d
Serkan IŞIK <dehnas79@gmail.com>
2023-10-26 17:15:47 +0300

    Update NotFound component

--- client-app/src/features/errors/NotFound.tsx
+++ client-app/src/features/errors/NotFound.tsx
@@ -3,13 +3,13 @@ import { Button, Header, Icon, Segment } from "semantic-ui-react";
 
 export default function NotFound() {
     return (
-        <Segment>
+        <Segment placeholder>
             <Header icon>
                 <Icon name='search' />
                 Oops - we've looked everywhere but could not find what you are looking for!
             </Header>
             <Segment.Inline>
-                <Button as={Link} to='/activities' primary>
+                <Button as={Link} to='/activities'>
                     Return to Activities page
                 </Button>
             </Segment.Inline>       
