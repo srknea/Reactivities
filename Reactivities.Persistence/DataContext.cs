f59c97c9aa7784929ddd0163c05f0418330eaf62
Serkan IŞIK <dehnas79@gmail.com>
2023-10-31 07:45:21 +0300

    Adding an IdentityDbContext

--- Reactivities.Persistence/DataContext.cs
+++ Reactivities.Persistence/DataContext.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain;
 
 namespace Reactivities.Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
