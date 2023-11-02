b11c4c5c7f522cbab8547a078ee3dc16dec11f37
Serkan IŞIK <dehnas79@gmail.com>
2023-11-02 09:32:57 +0300

    Adding an account controller

---
+++ Reactivities.API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.API.DTOs;
+using Reactivities.Domain;
+
+namespace Reactivities.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+            
+            if(user == null) return Unauthorized();
+
+            var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if(result)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "This will be a token",
+                    UserName = user.UserName
+                };
+            }
+
+            return Unauthorized();
+        }
+    }
+}
