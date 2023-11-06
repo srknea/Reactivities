468f07e8669ecefdac67b36f12d9b5fa3337e1bc
Serkan IŞIK <dehnas79@gmail.com>
2023-11-06 08:31:28 +0300

    Validating the registration of users

--- Reactivities.API/DTOs/RegisterDto.cs
+++ Reactivities.API/DTOs/RegisterDto.cs
@@ -1,10 +1,18 @@
-﻿namespace Reactivities.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reactivities.API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).(4,8)$", ErrorMessage = "Password must be complex")]
         public string Password { get; set;}
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
         public string UserName { get; set; }
     }
 }
