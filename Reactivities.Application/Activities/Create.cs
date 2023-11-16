53924ece2e3d895bbb53f2d3e562ba4e6b95669c
Serkan IŞIK <dehnas79@gmail.com>
2023-11-16 12:24:00 +0300

    Updating the create activity handler

--- Reactivities.Application/Activities/Create.cs
+++ Reactivities.Application/Activities/Create.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Reactivities.Application.Core;
+using Reactivities.Application.Interfaces;
 using Reactivities.Domain;
 using Reactivities.Persistence;
 using System;
@@ -29,14 +31,27 @@ namespace Reactivities.Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor = null)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+                
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activitiy,
+                    IsHost = true
+                };
+
+                request.Activitiy.Attendees.Add(attendee);
+
                 _context.Activities.Add(request.Activitiy);
 
                 var result = await _context.SaveChangesAsync() > 0;
