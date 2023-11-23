fd90f620989e96548ab9659dfbcbec8b1c1cb16c
Serkan IŞIK <dehnas79@gmail.com>
2023-11-23 18:31:01 +0300

    Updating the create and edit methods

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,7 +1,7 @@
 import { is } from "date-fns/locale"
 import { Profile } from "./profile"
 
-export interface Activity {
+export interface IActivity {
     id: string
     title: string
     date: Date | null
@@ -9,11 +9,58 @@ export interface Activity {
     category: string
     city: string
     venue: string
-    hostUsername?: string
-    isCancelled?: boolean
-    isGoing?: boolean
-    isHost?: boolean
+    hostUsername: string
+    isCancelled: boolean
+    isGoing: boolean
+    isHost: boolean
     host?: Profile
-    attendees?: Profile[]
+    attendees: Profile[]
 }
-  
+
+export class Activity implements IActivity {
+    constructor(init: ActivityFormValues) {
+        this.id = init.id!;
+        this.title = init.title;
+        this.date = init.date;
+        this.description = init.description;
+        this.category = init.category;
+        this.venue = init.venue;
+        this.city = init.city;
+    }
+
+    id: string
+    title: string
+    date: Date | null
+    description: string
+    category: string
+    city: string
+    venue: string
+    hostUsername: string = ''
+    isCancelled: boolean = false
+    isGoing: boolean = false
+    isHost: boolean = false
+    host?: Profile = undefined
+    attendees: Profile[] = []
+}
+
+export class ActivityFormValues {
+    id?: string = undefined;
+    title: string = '';
+    category: string = '';
+    description: string = '';
+    date: Date | null = null;
+    city: string = '';
+    venue: string = '';
+
+    constructor(activity?: ActivityFormValues) {
+        if (activity) {
+            this.id = activity.id;
+            this.title = activity.title;
+            this.category = activity.category;
+            this.description = activity.description;
+            this.date = activity.date;
+            this.city = activity.city;
+            this.venue = activity.venue;
+        }
+    }
+}
