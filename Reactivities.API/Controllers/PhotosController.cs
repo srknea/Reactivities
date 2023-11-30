00fe0cc983c48aaf7c0ccf07458128881c01137b
Serkan IŞIK <dehnas79@gmail.com>
2023-11-30 15:03:25 +0300

    Adding a photos controller

---
+++ Reactivities.API/Controllers/PhotosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Photos;
+
+namespace Reactivities.API.Controllers
+{
+    public class PhotosController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult> Add([FromForm] Add.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }        
+    }
+}
