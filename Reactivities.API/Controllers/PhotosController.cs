25abfd2835736e67a5e5162023b8cf7d40984cd6
Serkan IŞIK <dehnas79@gmail.com>
2023-11-30 15:20:08 +0300

    Deleting photos

--- Reactivities.API/Controllers/PhotosController.cs
+++ Reactivities.API/Controllers/PhotosController.cs
@@ -10,6 +10,12 @@ namespace Reactivities.API.Controllers
         public async Task<ActionResult> Add([FromForm] Add.Command command)
         {
             return HandleResult(await Mediator.Send(command));
-        }        
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
+        }
     }
 }
---
+++ Reactivities.Application/Photos/Delete.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Core;
+using Reactivities.Application.Interfaces;
+using Reactivities.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Photos
+{
+    public class Delete
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IPhotoAccessor _photoAccessor;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context = null, IPhotoAccessor photoAccessor = null, IUserAccessor userAccessor = null)
+            {
+                _context = context;
+                _photoAccessor = photoAccessor;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(p => p.Photos)
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if (user == null) return null;
+
+                var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+
+                if (photo == null) return null;
+
+                if (photo.IsMain) return Result<Unit>.Failure("You cannot delete your main photo");
+
+                var result = await _photoAccessor.DeletePhoto(photo.Id);
+
+                if (result == null) return Result<Unit>.Failure("Prroblem deleting photo from Coludinary");
+
+                user.Photos.Remove(photo);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Problem deleting photo from API");
+            }
+        }
+    }
+}
