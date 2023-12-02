42d09016902b63f264841c1378862928269fa669
Serkan IŞIK <dehnas79@gmail.com>
2023-12-02 13:50:43 +0300

    Returning user profiles

---
+++ Reactivities.API/Controllers/ProfilesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Profiles;
+
+namespace Reactivities.API.Controllers
+{
+    public class ProfilesController : BaseApiController
+    {
+        [HttpGet("{username}")]
+        public async Task<ActionResult<Profile>> Get(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+    }
+}
