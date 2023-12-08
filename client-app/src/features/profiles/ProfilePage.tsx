a4a4517fb099b85437359263e45c0c29fe935452
Serkan IŞIK <dehnas79@gmail.com>
2023-12-08 19:43:26 +0300

    Adding the profile content

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,11 +1,14 @@
 import { Grid } from "semantic-ui-react";
 import ProfileHeader from "./ProfileHeader";
+import { Profile } from "../../app/models/profile";
+import ProfileContent from "./ProfileContent";
 
 export default function ProfilePage() {
     return (
        <Grid>
             <Grid.Column width={16}>
                 <ProfileHeader/>
+                <ProfileContent/>
             </Grid.Column>    
        </Grid>
     )
